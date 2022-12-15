6c627ce588b9988f2871748e92c84ab0a7c3dfe1
xelilzade02 <xelilzadeismayil48@gmail.com>
2022-12-15 20:57:38 +0400

    Adddddd

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+
 fs.readFile('./data.json', 'utf8', (err, data) => {
     if (!err) {
-        app.get('/buy/:id', function (req, res) {
-            const idOfUser = parseInt(req.params.id);
-            const user = JSON.parse(data).find((user) => user.id === idOfUser);
-            if (!user) {
-                res.status(404).send();
-            }
-            res.status(200).json(user);
+        app.get('/get', function (req, res) {
+            res.send(data);
         });
         app.listen(3000, function () {
             console.log('Example app listening on port 3000!');
         });
-
     } else {
         console.error(err);
     }
